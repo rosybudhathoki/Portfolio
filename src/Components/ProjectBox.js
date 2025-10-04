064128bcbb55ac8c2c8e282743abae6e021b2ada
rosybudhathoki <budhatho@ualberta.ca>
2025-10-03 20:54:45 -0600

    added languages to prohject

--- src/Components/ProjectBox.js
+++ src/Components/ProjectBox.js
@@ -6,18 +6,23 @@ import {CgFileDocument} from "react-icons/cg";
 const  ProjectBox = ({projectPhoto, projectName}) => {
   const desc = {
     ScansagaDesc : "This is a firebase-powered Android app for event management with QR code check-ins, real-time attendance tracking, multimedia support, and geolocation sharing.",
+    ScansagaLanguages : "Java, XML, Firebase, AndrioStudio, JUnit",
+    ScansagaGithub : "https://github.com/rosybudhathoki/ScanSaga",
     ScansagaGithub : "https://github.com/rosybudhathoki/ScanSaga",
     ScansagaWebsite : "https://github.com/rosybudhathoki/ScanSaga",
 
-    TasklyDesc : "This project is a web-based to-do list application with cute aesthetics that helps you stay organized. User can add, delete, and modify tasks. It also has a stopwatch, and a timer feature.",
+    TasklyDesc : "This project is a web-based to-do list application with cute aesthetics that helps you stay organized. User can add, delete, modify and flter tasks. It also has a stopwatch, and a timer feature.",
+    TasklyLanguages : "HTML, CSS, JavaScript",
     TasklyGithub : "https://github.com/rosybudhathoki/Taskly",
     TasklyWebsite : "https://rosybudhathoki.github.io/Taskly/",
 
-    SudokuSolverDesc: "A Python-based Sudoku solver that can solve any Suduko under a second. Testsed on 95 complex suduko problems using two diffent algorithms to evaluate which one is efficient.",
+    SudokuSolverDesc: "A Python-based Sudoku solver that can solve any Suduko within seconds. Testsed on 95 complex suduko problems using two diffent algorithms to evaluate which one is efficient.",
+    SudokuSolverLanguages : "Python with libraries: Numpy, Matplotlib",
     SudokuSolverGithub:"https://github.com/rosybudhathoki/SudukoSolver",
     SudokuSolverWebsite:"https://github.com/rosybudhathoki/SudokuSolver/blob/main/finalResult.md",
 
     DatabaseMeasureDesc:"This project is a Python-based database benchmarking tool that measures MongoDB performance by analyzing query efficiency across databases of different sizes, with and without indexing.",
+    DatabaseMeasureLanguages : "Python, MongoDb",
     DatabaseMeasureGithub:"https://github.com/rosybudhathoki/DatabasePerformanceMeasure",
     DatabaseMeasureWebsite:"https://github.com/rosybudhathoki/DatabasePerformanceMeasure",
   }
@@ -36,15 +41,32 @@ const  ProjectBox = ({projectPhoto, projectName}) => {
             <br />
               <span style=
               {{
-                fontSize: '20px',       // Bigger text
-                fontWeight: '500',      // Medium-bold for clarity
-                lineHeight: '1.5',      // Better spacing between lines
-                fontFamily: 'Arial, sans-serif',  // Clean, readable font
-                color: '#ffffffff',               // Text color
+                fontSize: '20px',       
+                fontWeight: '500',     
+                lineHeight: '1.5',    
+                fontFamily: 'Arial, sans-serif',
+                color: '#ffffffff',              
               }}>
                 {desc[projectName + 'Desc']}
               </span>
             <br />
+
+            <br />
+              <span
+                style={{
+                  fontSize: '18px',
+                  fontWeight: '500',
+                  lineHeight: '1.5',
+                  fontFamily: 'Arial, sans-serif',
+                  color: '#ffffff',
+                  backgroundColor: 'rgba(0, 0, 0, 0.5)', 
+                  padding: '4px 8px',              
+                  borderRadius: '4px',                  
+                }}
+              >
+                {desc[projectName + 'Languages']}
+              </span>
+              <br />
 
             <a style={{display:show}} href={desc[projectName + 'Github']} target='_blank'>
               <button className='projectbtn'><FaGithub/> Github</button>
@@ -53,6 +75,7 @@ const  ProjectBox = ({projectPhoto, projectName}) => {
             <a href={desc[projectName + 'Website']} target='_blank'>
               <button className='projectbtn'><CgFileDocument/> Demo</button>
             </a>
+
         </div>
     </div>
   )
